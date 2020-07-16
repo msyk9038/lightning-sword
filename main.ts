5f6a0ed0a9fc797c14b054dff3fd5108d21274eb
msykmyt <41169937+msykmyt@users.noreply.github.com>
2020-07-17 07:32:48 +0900

    ベーシックな形

--- main.ts
+++ main.ts
@@ -1,21 +1,14 @@
 input.onGesture(Gesture.TiltRight, function () {
-    range0.showColor(neopixel.colors(NeoPixelColors.Blue))
-    range1.showColor(neopixel.colors(NeoPixelColors.Green))
+    strip.showColor(neopixel.colors(NeoPixelColors.Blue))
 })
 input.onGesture(Gesture.TiltLeft, function () {
-    range0.showColor(neopixel.colors(NeoPixelColors.Green))
-    range1.showColor(neopixel.colors(NeoPixelColors.Blue))
+    strip.showColor(neopixel.colors(NeoPixelColors.Green))
 })
 input.onGesture(Gesture.LogoUp, function () {
-    range0.showColor(neopixel.colors(NeoPixelColors.Red))
-    range1.showColor(neopixel.colors(NeoPixelColors.Blue))
+    strip.showColor(neopixel.colors(NeoPixelColors.Red))
 })
 input.onGesture(Gesture.LogoDown, function () {
-    range0.showColor(neopixel.colors(NeoPixelColors.Yellow))
-    range1.showColor(neopixel.colors(NeoPixelColors.Blue))
+    strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
 })
-let range1: neopixel.Strip = null
-let range0: neopixel.Strip = null
-let strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
-range0 = strip.range(0, 7)
-range1 = strip.range(7, 8)
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
